1ce18b3eb3e41e5a70cfe244983abbf26773b811
Pratham821 <115334452+PrathmeshNikam821@users.noreply.github.com>
2024-05-16 19:37:22 +0530

    Patterns part 1

---
+++ 05.PATTERN 1/invert_star.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class invert_star {
+    public static void main(String[] args){
+        for(int i = 1; i<=4; i++){
+            for(int j =1; j<=4-i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
